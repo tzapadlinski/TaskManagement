11bd4bb88620a61dc6079a4d7e3fdbefa0c7d54b
tzapadlinski <79110657+tzapadlinski@users.noreply.github.com>
2022-06-03 23:54:23 +0200

    Working on logging and accounts

---
+++ src/main/java/com/structure/Account.java
@@ -0,0 +1,39 @@
+package com.structure;
+
+import java.util.Objects;
+
+public class Account {
+    private String login;
+    private String password;
+    private Employee owner;
+
+    public Account(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Employee getOwner() {
+        return owner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(login, account.login) && Objects.equals(password, account.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+}
--- src/main/java/com/structure/ServiceAccess.java
+++ src/main/java/com/structure/ServiceAccess.java
@@ -1,7 +1,27 @@
 package com.structure;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ServiceAccess {
-    //passwordBase
-    //loginBase
-    //dupa
+    private ArrayList<Account> accounts;
+
+    ServiceAccess(){
+        accounts = new ArrayList<>();
+    }
+
+    //TODO disable adding accounts with same logins
+    public boolean addAccount(Account account, Employee employee){
+        accounts.add(account);
+        return true;
+    }
+
+    public Employee accountInBase(Account checked){
+        for (Account existing:accounts) {
+            if(checked.equals(existing))
+                return existing.getOwner();
+        }
+        return null;
+    }
+
 }
