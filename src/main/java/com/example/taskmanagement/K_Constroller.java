c811435b7b158947b53ba410b4c255859aa9d89f
kubq01 <kuba18062001@gmail.com>
2022-06-03 15:10:30 +0200

    klasa statyczna

---
+++ src/main/java/com/example/taskmanagement/K_Constroller.java
@@ -0,0 +1,5 @@
+package com.example.taskmanagement;
+
+public class K_Constroller {
+
+}
---
+++ src/main/java/com/structure/StaticContainer.java
@@ -0,0 +1,14 @@
+package com.structure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticContainer {
+	
+	static List<String> raportList = new ArrayList<>();
+	static List<Project> projectList = new ArrayList<>();
+	static List<Module> moduleList = new ArrayList<>();
+	static List<Task> taskList = new ArrayList<>();
+	static List<Employee> employeeList = new ArrayList<>();
+
+}
