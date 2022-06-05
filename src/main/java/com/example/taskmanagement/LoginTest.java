d1fb29920749366b116d1f2a8aa6bf2e4db41255
tzapadlinski <79110657+tzapadlinski@users.noreply.github.com>
2022-06-05 22:32:12 +0200

    Prawie skończyłem logowanie.

--- src/main/java/com/example/taskmanagement/LoginTest.java
+++ src/main/java/com/example/taskmanagement/LoginTest.java
@@ -1,9 +1,9 @@
 package com.example.taskmanagement;
-import com.structure.Employee;
+import com.structure.*;
 
-import com.structure.Position;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -11,20 +11,34 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginTest extends Application {
+
+    private Stage stg;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginBox.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        stg = stage;
+        stage.setResizable(false);
+        Parent root = FXMLLoader.load(getClass().getResource("loginBox.fxml"));
         stage.getIcons().add(new Image("file:src\\main\\resources" +
                 "\\com\\example\\taskmanagement\\images\\logo.png")); //!here's path to images!
-        stage.setTitle("Log in to our service!");
-        stage.setScene(scene);
+        stage.setTitle("Jira 2.0");
+        stage.setScene(new Scene(root));
         stage.show();
-
 
     }
 
     public static void main(String[] args) {
+        ServiceAccess.addAccount(new Account("adam", "malysz"),
+                new Manager(1, "Adam", "Malysz"));
+        ServiceAccess.addAccount(new Account("mario", "pudzian"),
+                new Worker(2, "Mariusz", "Pudzianowski", Position.TESTER));
+        ServiceAccess.addAccount(new Account("kubica", "kubica"),
+                new Worker(3, "Robert", "Kubica", Position.PROGRAMISTA));
         launch();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 }
