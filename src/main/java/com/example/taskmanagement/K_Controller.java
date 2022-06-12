d43eab5b9360e7f1648a5a7fb59f69ca400ea40b
tzapadlinski <79110657+tzapadlinski@users.noreply.github.com>
2022-06-12 16:58:28 +0200

    działa

--- src/main/java/com/example/taskmanagement/K_Controller.java
+++ src/main/java/com/example/taskmanagement/K_Controller.java
@@ -722,9 +722,9 @@ public class K_Controller {
     //Czesc, tu Tomek
 	public void setRaporty(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("logScene.fxml"));
-		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
+		Stage stage = new Stage();
+		stage.setTitle("Raport");
+		stage.setScene(new Scene(root));
 		stage.show();
 	}
     
